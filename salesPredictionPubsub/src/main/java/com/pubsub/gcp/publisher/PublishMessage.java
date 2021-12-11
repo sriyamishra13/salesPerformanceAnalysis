077053f07ba4836494204b4af0d0c5ee8b288cf0
sriyamishra13 <86830858+sriyamishra13@users.noreply.github.com>
2021-12-10 23:01:13 -0600

    Create PublishMessage.java

---
+++ salesPredictionPubsub/src/main/java/com/pubsub/gcp/publisher/PublishMessage.java
@@ -0,0 +1,39 @@
+package com.pubsub.gcp.publisher;
+
+import com.google.cloud.spring.pubsub.core.publisher.PubSubPublisherTemplate;
+
+import jdk.jfr.internal.Logger;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.ExecutionException;
+
+@RestController
+public class PublishMessage {
+
+    private final static Log LOGGER = LogFactory.getLog(PublishMessage.class);
+
+    @Autowired
+    private final PubSubPublisherTemplate pubSubPublisherTemplate;
+
+    @Autowired
+    public ProductService productService;
+
+    PublishMessage(PubSubPublisherTemplate pubSubPublisherTemplate) {
+       this.pubSubPublisherTemplate = pubSubPublisherTemplate;
+    }
+
+    @PostMapping("/product-details")
+    void pushProductDetails(@RequestBody ProductData message) throws ExecutionException, InterruptedException {
+        LOGGER.info("Message received = " + message);
+        String messageId = pubSubPublisherTemplate.publish("product-pubsub", message).get();
+        LOGGER.info("Message published with ID: " + messageId);
+        productService.persistProductDetails(message);
+    }
+
+}
